54469ede9883132ef123c7b288bbc5f99f8f7490
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-06-06 22:32:00 -0400

    Add all files without compliments in array

---
+++ compliments.js
@@ -0,0 +1,3 @@
+var complimentArray = ["You're pretty", "You're SO pretty", "You're SUPER pretty"];
+var random = Math.floor(Math.random() * complimentArray.length);
+var newCompliment = complimentArray[random];
