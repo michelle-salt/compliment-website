a416e045a62ea2a5cb5bc54095d4b8341c44732c
michelle-salt <69985466+michelle-salt@users.noreply.github.com>
2021-06-06 22:35:03 -0400

    Delete compliments.js

--- compliments.js
+++
@@ -1,3 +0,0 @@
-var complimentArray = ["You're pretty", "You're SO pretty", "You're SUPER pretty"];
-var random = Math.floor(Math.random() * complimentArray.length);
-var newCompliment = complimentArray[random];
